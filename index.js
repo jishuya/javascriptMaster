fa085cc510379683503673313007edef245e248f
jslim <71686380+jishuya@users.noreply.github.com>
2022-06-01 16:31:37 +0900

    javascipt basic logic

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('gggggg')
+let apple = {
+    name: '사과',
+    price: 2000
+};
+
+let orange = apple;
+
+console.log(apple)
+console.log(orange)
+
+orange.name = '오렌지'
+console.log(apple)
+console.log(orange)
