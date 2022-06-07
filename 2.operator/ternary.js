567412ef91b6abae2eec66caa577770ec1c36b55
jslim <71686380+jishuya@users.noreply.github.com>
2022-06-08 08:53:28 +0900

    operator

---
+++ 2.operator/ternary.js
@@ -0,0 +1,11 @@
+let fruit = 'apple';
+if (fruit === 'apple'){
+    console.log('🍎')
+} else {
+    console.log('🍊')
+}
+
+fruit === 'apple'? console.log('🍎'):console.log('🍊');
+
+let emoji = fruit === 'apple'? console.log('🍎'):console.log('🍊');
+console.log(emoji)
