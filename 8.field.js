9756b9ed36f3efc8d3c75c751571361cb5a93bb6
jishuya <jishuya3015@gmail.com>
2022-09-10 13:30:24 +0900

    class study again

--- 8.field.js
+++ 8.field.js
@@ -22,11 +22,28 @@ class Student {
     constructor(name, age) {
         this.name = name;
         this.age = age;
+        // this.selfIntroduction = `I'm ${this.name}. I'm ${this.age}yaers old.`;
+    }
+    get selfIntroduction(){
+        return `I'm ${this.name}. I'm ${this.age}yaers old.`;
+    }
+    set selfIntroduction(value){
+        console.log('set', value)
     }
     introduce() {
-        return `I', ${this.name}. I'm ${this.age}yaers old.`;
+        return `I'm ${this.name}. I'm ${this.age}yaers old.`;
     }
 }
 
 const hyena = new Student('hyena', 25);
-console.log(hyena.introduce);
+// console.log(hyena.introduce());
+// 생성자가 생성된 다음에 속성 바꾸기
+// selfIntroduction에 접근할 때마다 업데이트 된 걸로 접근하고 싶다면? 
+// get을 사용한다. 
+hyena.age = 30;
+console.log(111, hyena.age);
+console.log(222, hyena.selfIntroduction);
+
+// set
+hyena.selfIntroduction = 'siwon';
+
