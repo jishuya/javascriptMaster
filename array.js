825635405d87f3e4d4515cf837a0e939f795dcf9
jishuya <jishuya3015@gmail.com>
2022-09-20 09:11:56 +0900

    array Study2

--- array.js
+++ array.js
@@ -1,92 +1,47 @@
-let array = new Array(3);
-console.log(array)
 
-array = new Array(1, 2, 3)
-console.log(array);
-
-
-
-// var , let , const 
-// 재선언, 재할당 
-
-array = Array.of(8)
-console.log(array);
-const array2 = [1, 2, 3, 4, 5]
-console.log(array2);
-
-const array3 = Array.from('text');
-console.log(array3);
-
-array = Array.from({
-    0: '안',
-    1: '녕',
-    length : 2
-})
-console.log(array);
-
-console.clear();
-
-// 마지막에 추가하기 
-const food = ['🍕', '🍔', '🌭', '🍩', '🥨'];
-console.log(food.length);
-food[food.length] = '🥪'
+/** 새로운 배열을 리턴 */
+// 1. 자르기 : slice(시작인덱스, 끝인덱스 -1(자기자신 미포함))
+const food = ['🍕', '🍔', '🌭', '🍩'];
+let newArr = food.slice();
+let newArr2 = food.slice(1, 3);
+let newArr3 = food.slice(1);    // 종료 인덱스 안주면 시작인덱스 ~ 끝까지
+let newArr4 = food.slice(-2);
 console.log(food);
+console.log(newArr);        //  [ '🍕', '🍔', '🌭', '🍩' ]
+console.log(newArr2);       //  ['🍔', '🌭' ]
+console.log(newArr3);       //  [ '🍔', '🌭', '🍩' ]
+console.log(newArr4);       //  [ '🌭', '🍩' ]
 
-// 삭제할 때 인덱스를 바로 사용하게 되면 중간에 배열이 비어버려서 좋지 않다
-delete food[1];
-console.log(food);
+// 2. 붙이기 : contact 
+const arr1 = ['H', 'E'];
+const arr2 = ['L', 'L', 'O'];
+const concatArr = arr1.concat(arr2);
+console.log(concatArr);          // [ 'H', 'E', 'L', 'L', 'O' ]
 
-//  배열 자체를 변경할 수 있는 함수는 ?
-array = Array.of(8)
-console.log(array);
-const array2 = [1, 2, 3, 4, 5]
-console.log(array2);
+// 3. 순서 뒤집기 : reverse
+const reverseArr = concatArr.reverse();
+console.log(reverseArr);        // [ 'O', 'L', 'L', 'E', 'H' ]
 
-const array3 = Array.from('text');
-console.log(array3);
-
-array = Array.from({
-    0: '안',
-    1: '녕',
-    length : 2
-})
-console.log(array);
-
-console.clear();
-
-// 마지막에 추가하기 
-const food = ['🍕', '🍔', '🌭', '🍩', '🥨'];
-console.log(food.length);
-food[food.length] = '🥪'
-console.log(food);
-
-// 삭제할 때 인덱스를 바로 사용하게 되면 중간에 배열이 비어버려서 좋지 않다
-delete food[1];
-console.log(food);
+// 4. 중첩배열 펼치기 : flat
+const sample = [ [1, 2,], 3, [4, [5, 6]]];
+const flatArr = sample.flat();
+const flatArr2 = flatArr.flat();
+console.log(flatArr);           // [ 1, 2, 3, 4, [ 5, 6 ] ]
+console.log(flatArr2);          // [ 1, 2, 3, 4, 5, 6 ]
 
 
-/** 배열 자체를 수정 */
+/**원본 배열 직접 변경*/
 
-// 추가하기
-// 1. unshift : 맨 앞
-let length = menu.unshift('🥗');      
-console.log(menu);      // [ '🥗', '🍕', '🍔', '🍿', '🌭' ]
-// 2. push : 맨 뒤
-length = menu.push('🍖');
-console.log(menu);      // [ '🥗', '🍕', '🍔', '🍿', '🌭', '🍖' ]
+// 채우기 : fill(채울값, 시작인덱스, 끝인덱스-1(자기자신 미포함))
+const nullArray = [null, null, null, null, null];
+nullArray.fill(0);
+console.log(nullArray);         // [ 0, 0, 0, 0, 0 ]
+nullArray.fill('❤', 1, 3)
+console.log(nullArray);         // [ 0, '❤', '❤', 0, 0 ]
 
+// 배열의 요소를 문자로 합치기 
+const joinArr = ['안녕', '하세요', '😉']
+console.log(joinArr.join());            // 안녕,하세요,😉
+console.log(joinArr.join(''));          // 안녕하세요😉
+console.log(joinArr.join('&&'));        // 안녕&&하세요&&😉
 
-// 제거하기
-// 1. shift : 맨 앞
-let removedMenu = menu.shift();      
-console.log(removedMenu);       // 🥗
-console.log(menu);      // [ '🍕', '🍔', '🍿', '🌭', '🍖' ]
-// 2. pop : 맨 뒤 
-removedMenu = menu.pop();
-console.log(removedMenu);       // 🍖   
-console.log(menu);      // [ '🍕', '🍔', '🍿', '🌭' ]
-// 3. splice(몇번째부터 자를지, 몇개 자를지, 추가한다면 추가할 요소) : 중간요소 제거/추가
-menu.splice(2, 1); 
-console.log(menu);      // [ '🍕', '🍔', '🌭' ]
-menu.splice(1, 1, '🍘', '🍛');      
-console.log(menu);      // [ '🍕', '🍘', '🍛', '🌭' ]
