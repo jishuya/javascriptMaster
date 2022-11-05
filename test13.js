3113a97e8934f3be5f167376b6336017ff15a7e2
jishuya <jishuya3015@gmail.com>
2022-11-05 21:01:25 +0900

    closure test

--- test13.js
+++ test13.js
@@ -1,36 +1,53 @@
-const quiz1 = {
-    question: '다음 중 스승의 날과 생일이 같은 인물은 누구일까요?',
-    example: [ '율곡 이이', '퇴계 이황', '세종대왕', '신사임당'],
-    answer: 3,
-    solution: '훈민정음 창제 등 우리나라 문화와 교육발전에 남긴 업적이 가장 큰 인물이라는 평가와 함께, 이 시대의 스승이 세종대왕처럼 존경받았으면 하는 바램으로 세종대왕의 탄신일이 스승의 날로 지정되었습니다.',
-  };
-  
-  const quiz2 = {
-    question: '다음 중 희노애락에 속하지 않는 것은 무엇일까요?',
-    example: ['사랑', '기쁨', '즐거움'],
-    answer: 1,
-    solution: '희노애락에서의 애를 사랑 애(愛)라고 자칫 오해하는 경우가 있지만, 희노애락의 애는 슬플 애(哀)입니다. 기쁨은 기쁠 희(喜), 즐거움은 즐거울 락(樂)에 담겨 있습니다.',
-  };
-  
-  function printQuiz({ question, example }) {
-    try {
-      let exMsg = '';
-  
-      for (let i = 0; i < example.length; i++) {
-        exMsg += `${i + 1}. ${example[i]}  `;
-      }
-  
-      console.log(question);
-      console.log(exMsg);
-    } catch (e) {
-      console.log(e.name);
-    
-    }
+// function loop(){
+//     const array = [];
+//     // var와 let의 차이점
+//     for (let i = 0; i <5; i++){
+//         array.push(function(){
+//             console.log(i);
+//         });
+//     }
+//     return array;
+// }
+
+// const array = loop();
+// array.forEach((func)=>func());
+
+
+
+
+var apple = '사과';
+{
+  var apple = '🍎';
+  {
+    var apple = '🍏';
+  }
+}
+console.log(apple);
+
+{
+    var x = 3;
   }
   
-  // 테스트 코드
-  printQuiz(quiz1);
-  printQuiz(1);
-  printQuiz("");
-  printQuiz({});
-  printQuiz(quiz2);
+  function myFunction() {
+    var y = 4;
+  }
+  
+  console.log(x);
+//   console.log(y);
+
+
+
+function loop(){
+    const array = [];
+    // var와 let의 차이점
+    var i;
+    for (i = 0; i <5; i++){
+        array.push(function(){
+            console.log(i);
+        });
+    }
+    return array;
+}
+
+const array = loop();
+array.forEach((func)=>func());
