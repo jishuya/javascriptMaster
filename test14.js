42461197aef9d40fe95f445448c1eeb5fcc6c752
jishuya <jishuya3015@gmail.com>
2022-11-08 21:05:17 +0900

    arrow function

--- test14.js
+++ test14.js
@@ -1,45 +1,55 @@
-console.log(this);
-const x =0;
-module.exports.x = x;
-console.log(this);
-console.log(globalThis);
+// // const aa = 4;
 
-'use strict'
-function fun(){
-  console.log(this)
+// // module.exports.x = aa;
+
+// // console.log(this);
+// // console.log(globalThis);
+
+// // 'use strict'
+
+// function fun(){
+//   console.log(this);
+// }
+
+// fun();
+
+// console.clear();
+
+
+function Cat(name){
+  this.name = name
+  this.printName = ()=>{
+    console.log(`고양이 이름: ${this.name}`)
+  };
+  // this.printName = this.printName.bind(this)
 }
 
-fun()
 
-function Cat(name) {
-  this.name = name;
-  this.printName = function () {
-    console.log(`고양이의 이름을 출력한다옹: ${this.name}`);
-  };
-}
-
-function Dog(name) {
-  this.name = name;
-  this.printName = function () {
-    console.log(`강아지의 이름을 출력한다옹: ${this.name}`);
-  };
+function Dog(name){
+  this.name = name
+  this.printName = ()=>{
+    console.log(`강아지 이름: ${this.name}`)
+  }
+  // this.printName = this.printName.bind(this)
 }
 
 const cat = new Cat('냐옹');
 const dog = new Dog('멍멍');
-cat.printName();
+
+// cat.printName()
+// dog.printName()
+
+// dog.printName = cat.printName;
+// 이제 cat도 강아지 이름으로 바뀜
+cat.printName = dog.printName;
+
 dog.printName();
-
-dog.printName = cat.printName;
-dog.printName();
 cat.printName();
-// 호출한 사람 dog라서 this는 dog, printName함수에 cat.PrintName바인딩해줘서 '고양이 이름을 출력한다옹'
-// cat.printName();
 
 
-function printOn(printName){
-  console.log('모니터 준비하고 전달된 콜백 실행해줘!')
+function printMonitoring(printName){
+  console.log('모니터 준비하고 콜백을 실행')
   printName();
 }
 
-printOn(cat.printName);
+printMonitoring(cat.printName);
