3e1e85fa4129cf4bf6f76910250f542c05568aee
jishuya <jishuya3015@gmail.com>
2022-11-17 21:56:18 +0900

    git test

--- test14.js
+++ test14.js
@@ -1,1 +1,46 @@
-console.log('git reset test')
+let num = 1
+switch(number){
+    case 1:
+        console.log('hi')
+    break;
+    default:
+        console.log('Hello!')
+    break;
+
+    
+}
+
+for (let i = 0;  i < 5; i++){
+    if(i === 3){
+        break;        
+    }
+    console.log(i)
+}
+
+
+
+for (let i = 0;  i < 6; i++){
+    if(i === 3){
+        continue;   
+    }
+    console.log(i)
+}
+
+
+
+// // while 문 : 조건이 false가 될 때까지 코드 반복
+let num = 5;
+while(num >= 4){
+    console.log(num);
+    num--;
+    
+};
+
+
+let limit = 0;
+do{
+    console.log('❤', limit)
+    limit++
+} while(limit < 3){
+    console.log(limit, '이제 끝')
+}
